0cd33e42f9dd317d1420dd7e6564cf4e0a1217cb
Jan Holderied <holderja@hs-albsig.de>
2023-04-28 15:10:50 +0200

    added git ignore

---
+++ Backend/public/Java Skript/script.js
@@ -0,0 +1,22 @@
+function openPopupImpressum() {
+    var modal = document.getElementById("modalImpressum");
+    modal.style.display = "block";
+  }
+
+function loggingin(){
+    var meta_user="admin";
+    var meta_pw="admin";
+    var user=document.getElementById("inputuser").value;
+    var password=document.getElementById("inputpassword").value
+
+    if(meta_user==user && meta_pw==password){
+      //weiterleitung auf produktverwaltung.html
+      //window.alert(window.location.href);
+      //window.location.assign("produktverwaltung.html")
+    }
+    else{
+      window.alert("Falsche Logindaten");
+      //window.location.href("http://www.google.de")
+    }
+
+}
