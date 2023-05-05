ffdafb22b13411374bb93a3d3bc45836e0f0f69c
Lukas Fabian Schwabenthan <schwabenthan1998@gmail.com>
2023-05-05 14:50:18 +0200

    auftraegeRouting.js ersellt

--- Backend/routes/auftraegeRouting.js
+++ Backend/routes/auftraegeRouting.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 // Create a router instance
 var router = express.Router();
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
+
+router.get("/reservierungen/akzeptieren", (req, res) =>{
+  res.status(200).send("Die Reservierung wurde akzeptiert");
+});
+
+router.get("/reservierungen/ablehnen", (req, res) =>{
+  res.status(200).send("Die Reservierung wurde abgelehnt");
+});
+
+router.get("/reservierungen", (req, res) =>{
+  res.status(200).send("Die Reservierungen wurden geladen");
+});
 
 router.post("/warenkorb", (req, res) => 
   {
